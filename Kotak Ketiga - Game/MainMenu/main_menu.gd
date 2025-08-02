c75b6f651a442353598001764106e7eaa53711e4
itsmikoo <skolastikakimiko@gmail.com>
2025-08-02 15:54:26 +0700

    update options & main menu

---
+++ Kotak Ketiga - Game/MainMenu/main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@onready var optionspanel: Panel = $optionspanel
+@onready var buttonstart: Button = $buttonstart
+@onready var buttonoption: Button = $buttonoption
+@onready var buttonclosepanel: Button = $optionspanel/buttonclosepanel
+
+
+func _ready():
+	buttonoption.visible = true
+	buttonstart.visible = true
+	optionspanel.visible = false
+
+func _on_buttonoption_pressed() -> void:
+	buttonoption.visible = false
+	buttonstart.visible = false
+	optionspanel.visible = true
+
+func _on_buttonclosepanel_pressed() -> void:
+	buttonoption.visible = true
+	buttonstart.visible = true
+	optionspanel.visible = false
+
+
+func _on_exitbutton_pressed() -> void:
+	get_tree().quit()
