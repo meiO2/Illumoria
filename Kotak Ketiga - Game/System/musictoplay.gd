414f9d0f11d80ea051fc3ac3305ca219f2c4916a
Gloriana Monica <glorianamonica06@gmail.com>
2025-08-10 23:08:54 +0700

    story Chapter2 and small changes

--- Kotak Ketiga - Game/System/musictoplay.gd
+++ Kotak Ketiga - Game/System/musictoplay.gd
@@ -2,5 +2,5 @@ extends Node
 
 func _ready():
 	var path = get_tree().current_scene.scene_file_path
-	if path.contains("res://Main-Game/Chapter2/Scene-Chapter2/Path-Kantor/StartPointChap2"):
+	if path.contains("res://Main-Game/Chapter2/Scene-Chapter2/Path-Kantor/StartPointChap2") or path.contains("res://Main-Game/Chapter1/Scene-Chapter1/PathKantor/Chapter1a"):
 		MusicOffice.playmusic()
