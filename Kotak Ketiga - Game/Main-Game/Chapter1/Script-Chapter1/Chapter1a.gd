97c34d458ff1935f952919842e0974c0ec000a1f
itsmikoo <skolastikakimiko@gmail.com>
2025-08-13 23:02:03 +0700

    update dialog txt

--- Kotak Ketiga - Game/Main-Game/Chapter1/Script-Chapter1/Chapter1a.gd
+++ Kotak Ketiga - Game/Main-Game/Chapter1/Script-Chapter1/Chapter1a.gd
@@ -1,91 +1,144 @@
 extends Node
+@onready var dialog_box = $CanvasLayer/DialogBox
 
-@onready var title = $Title
-@onready var label = $Label
-@onready var Option1 = $Option1
-@onready var Option2 = $Option2
 
-var entries = []
-var current_index = 0
-var waiting_for_choice = false
+var entries = [] 
+var current_path = ""
+var current_index = 0 
+var can_advance = false
+
+var story_stack = []
+var visited_choices = {}
 
 func _ready():
-	load_entries("res://Main-Game/Chapter1/Story-Chapter1/Chapter1a.txt")
-	show_entry(current_index)
+	dialog_box.dialog_finished.connect(_on_dialog_finished)
+	dialog_box.choice_made.connect(_on_choice_made)
+	
+	start_game()
+
+func _input(event):
+	if not dialog_box.visible:
+		return
+		
+	if event.is_action_pressed("ui_accept"):
+		if not dialog_box.timer.is_stopped():
+			dialog_box.complete_typing()
+		elif can_advance:
+			show_entry(current_index + 1)
+
+func start_game():
+	story_stack.clear()
+	visited_choices.clear()
+	var main_story_path = "res://Main-Game/Chapter1/Story-Chapter1/Chapter1a.txt"
+	load_and_show(main_story_path)
+
+func load_and_show(path):
+	current_path = path
+	load_entries(path)
+	show_entry(0) 
 
 func load_entries(path):
+	entries.clear()
 	var file = FileAccess.open(path, FileAccess.READ)
+	if not file:
+		print("ERROR: Gagal membuka file cerita di path: ", path)
+		return
+
 	while not file.eof_reached():
 		var line = file.get_line().strip_edges()
-		if line == "":
-			continue
+		if line == "" or line.begins_with("#"): continue
 
 		if line.begins_with("$"):
-			var without_dollar = line.trim_prefix("$").strip_edges()
-			if "|" in without_dollar:
-				var detail = without_dollar.split("|", false, 2)
-				if detail.size() > 1:
-					entries.append({
-						"type": "dialog",
-						"name": detail[0].strip_edges(),
-						"text": detail[1].strip_edges() })
-
-		elif "-" in line:
-			var parts = line.split("-", false, 2)
-			if parts.size() > 1:
-				entries.append({
-					"type": "choice",
-					"op1": parts[0].strip_edges(),
-					"op2": parts[1].strip_edges() })
-
-		elif line.begins_with("#"):
-			continue
+			var detail = line.trim_prefix("$").strip_edges().split("|", false, 2)
+			if detail.size() < 2: continue
+			entries.append({"type": "dialog", "name": detail[0].strip_edges(), "text": detail[1].strip_edges()})
+		
+		elif "%" in line or "&" in line or line.begins_with("@"):
+			var choice_id = ""
 			
-		elif line.begins_with("#"):
-			if line == "*1":
-				pass
+			if line.begins_with("@"):
+				var colon_pos = line.find(":")
+				if colon_pos != -1:
+					choice_id = line.substr(0, colon_pos).trim_prefix("@").strip_edges()
+					line = line.substr(colon_pos + 1).strip_edges()
+
+			var separator = "%" if "%" in line else "&"
+			var parts = line.split(separator)
+
+			var choice_options = []
+			
+			for part in parts:
+				part = part.strip_edges()
+				var option_data = {"text": part, "action": "continue"}
+				if "->" in part:
+					var action_parts = part.split("->", false, 2)
+					option_data["text"] = action_parts[0].strip_edges()
+					option_data["action"] = "load_file"
+					option_data["path"] = action_parts[1].strip_edges()
+				choice_options.append(option_data)
+			
+			entries.append({"type": "choice", "id": choice_id, "options": choice_options})
 
 	file.close()
 
 func show_entry(index):
-	if index >= entries.size():
+	current_index = index
+	
+	if current_index >= entries.size():
+		if not story_stack.is_empty():
+			var return_state = story_stack.pop_back()
+			load_and_show(return_state.path)
+			show_entry(return_state.index)
+		else:
+			dialog_box.hide_all()
 		return
 
-	var entry = entries[index]
+	can_advance = false
+	var entry = entries[current_index]
 
 	if entry["type"] == "dialog":
-		waiting_for_choice = false
-		title.text = entry["name"]
-		label.text = entry["text"]
-		Option1.visible = false
-		Option2.visible = false
+		dialog_box.show_dialog(entry["name"], entry["text"])
 
 	elif entry["type"] == "choice":
-		waiting_for_choice = true
-		Option1.text = entry["op1"]
-		Option2.text = entry["op2"]
-		Option1.visible = true
-		Option2.visible = true
+		var texts_for_buttons = []
+		var original_indices = []
+		
+		if entry.id != "" and entry.id in visited_choices:
+			var visited_indices = visited_choices[entry.id]
+			if visited_indices.size() == entry.options.size():
+				show_entry(current_index + 1)
+				return
+			
+			for i in range(entry.options.size()):
+				if not i in visited_indices:
+					texts_for_buttons.append(entry.options[i].text)
+					original_indices.append(i)
+		else:
+			for i in range(entry.options.size()):
+				texts_for_buttons.append(entry.options[i].text)
+				original_indices.append(i)
+		
+		entry["original_indices"] = original_indices
+		dialog_box.show_choices(texts_for_buttons)
 
-func _input(event):
-	if waiting_for_choice:
-		return
+func _on_dialog_finished():
+	can_advance = true
 
-	if event.is_action_pressed("ui_accept") \
-	or (event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT):
-		current_index += 1
-		show_entry(current_index)
-
-func _on_Option1_pressed():
-	waiting_for_choice = false
-	Option1.visible = false
-	Option2.visible = false
-	current_index += 1
-	show_entry(current_index)
-
-func _on_Option2_pressed():
-	waiting_for_choice = false
-	Option1.visible = false
-	Option2.visible = false
-	current_index += 1
-	show_entry(current_index)
+func _on_choice_made(filtered_index):
+	var entry = entries[current_index]
+	
+	var original_index = entry.original_indices[filtered_index]
+	var chosen_option = entry.options[original_index]
+	
+	if entry.id != "":
+		if not entry.id in visited_choices:
+			visited_choices[entry.id] = []
+		visited_choices[entry.id].append(original_index)
+		
+		story_stack.push_back({"path": current_path, "index": current_index})
+	
+	match chosen_option.action:
+		"load_file":
+			load_and_show(chosen_option.path)
+		"continue":
+			show_entry(current_index + 1)
