86947d25c553283affae82cdc62d573fdb6d3932
itsmikoo <skolastikakimiko@gmail.com>
2025-08-16 23:29:33 +0700

    benerin main menu 2

--- Kotak Ketiga - Game/ui/MainMenu/main_menu.gd
+++ Kotak Ketiga - Game/ui/MainMenu/main_menu.gd
@@ -4,20 +4,24 @@ extends Control
 @onready var buttonstart: Button = $buttonstart
 @onready var buttonoption: Button = $buttonoption
 @onready var buttonclosepanel: Button = $optionspanel/buttonclosepanel
+@onready var title: Label = $Title
 
 func _ready():
 	buttonoption.visible = true
 	buttonstart.visible = true
+	title.visible = true
 	optionspanel.visible = false
 
 func _on_buttonoption_pressed() -> void:
 	buttonoption.visible = false
 	buttonstart.visible = false
+	title.visible = false
 	optionspanel.visible = true
 
 func _on_buttonclosepanel_pressed() -> void:
 	buttonoption.visible = true
 	buttonstart.visible = true
+	title.visible = true
 	optionspanel.visible = false
 
 func _on_exitbutton_pressed() -> void:
