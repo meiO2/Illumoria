787dd8e834cff755d83613a370ace0ce18a32e5a
itsmikoo <skolastikakimiko@gmail.com>
2025-08-16 23:22:40 +0700

    benerin main menu

--- Kotak Ketiga - Game/ui/MainMenu/main_menu.gd
+++ Kotak Ketiga - Game/ui/MainMenu/main_menu.gd
@@ -5,7 +5,6 @@ extends Control
 @onready var buttonoption: Button = $buttonoption
 @onready var buttonclosepanel: Button = $optionspanel/buttonclosepanel
 
-
 func _ready():
 	buttonoption.visible = true
 	buttonstart.visible = true
@@ -21,10 +20,8 @@ func _on_buttonclosepanel_pressed() -> void:
 	buttonstart.visible = true
 	optionspanel.visible = false
 
-
 func _on_exitbutton_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_buttonstart_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main-Game/Cutscene/Opening/Cutscene-Opening.tscn")
