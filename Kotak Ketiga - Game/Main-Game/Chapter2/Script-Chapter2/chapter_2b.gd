3555c98555d0bf586818310f352f68c0ed5c2cdf
itsmikoo <skolastikakimiko@gmail.com>
2025-08-20 18:21:25 +0700

    puyeng

---
+++ Kotak Ketiga - Game/Main-Game/Chapter2/Script-Chapter2/chapter_2b.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var dialog_manager: Node
+
+func _ready():
+	if dialog_manager:
+		dialog_manager.all_dialog_finished.connect(_on_all_dialog_finished)
+		
+		var story_file = "res://Main-Game/Chapter3/Story-Chapter3/Chapter3a.txt"
+		dialog_manager.start_dialog_from_file(story_file)
+	else:
+		printerr("ERROR: DialogManager belum di-assign!")
+
+func _on_all_dialog_finished():
+	get_tree().change_scene_to_file("res://Main-Game/Chapter3/Scene-Chapter3/scenekantor/chapter3b.tscn") #gantiscene
